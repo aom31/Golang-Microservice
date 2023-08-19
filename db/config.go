0449ee00f771b74d8c62fc3c904f3be251479852
thamakorn.k <thamakorn.aom31@gmail.com>
2023-08-19 22:56:13 +0700

    update connection database and port

--- db/config.go
+++ db/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 type Config interface {
@@ -16,7 +15,7 @@ type config struct {
 	dbUser string
 	dbPass string
 	dbHost string
-	dbPort int
+	dbPort string
 	dbName string
 	dsn    string
 }
@@ -28,11 +27,11 @@ func NewConfig() Config {
 	cfg.dbHost = os.Getenv("DATABASE_HOST")
 	cfg.dbName = os.Getenv("DATABASE_NAME")
 	var err error
-	cfg.dbPort, err = strconv.Atoi(os.Getenv("DATABASE_PORT"))
+	cfg.dbPort = os.Getenv("DATABASE_PORT")
 	if err != nil {
 		log.Fatalln("Error on load env var:", err.Error())
 	}
-	cfg.dsn = fmt.Sprintf("mongodb://%s:%s@%s:%d/%s", cfg.dbUser, cfg.dbPass, cfg.dbHost, cfg.dbPort, cfg.dbName)
+	cfg.dsn = fmt.Sprintf("mongodb://%s:%s@%s:%s/%s", cfg.dbUser, cfg.dbPass, cfg.dbHost, cfg.dbPort, cfg.dbName)
 	return &cfg
 }
 
--- db/db.go
+++ db/db.go
@@ -17,7 +17,7 @@ type conn struct {
 }
 
 func NewConnection(cfg Config) (Connection, error) {
-	fmt.Println("database url:", cfg.Dsn())
+	fmt.Println("Success Connected database url:", cfg.Dsn())
 	session, err := mgo.Dial(cfg.Dsn())
 	if err != nil {
 		return nil, err
